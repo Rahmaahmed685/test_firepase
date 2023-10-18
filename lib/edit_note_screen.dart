f65372dbd23b5dd0487cfe9293ce9a044c1d1ba2
Rahmaahmed685 <rahmaahmed8276@email.com>
2023-10-19 00:49:55 +0300

    add_image

--- lib/edit_note_screen.dart
+++ lib/edit_note_screen.dart
@@ -6,28 +6,33 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:test_firepase/note.dart';
-class EditNoteScreen extends StatefulWidget {
-  const EditNoteScreen({super.key, required this.note});
+class Edit extends StatefulWidget {
+  const Edit({super.key, required this.note});
 
   final Note note;
 
   @override
-  State<EditNoteScreen> createState() => _EditNoteScreenState();
+  State<Edit> createState() => _EditState();
 }
 
-class _EditNoteScreenState extends State<EditNoteScreen> {
+class _EditState extends State<Edit> {
   final titleController = TextEditingController();
   final contentController = TextEditingController();
 
+  final auth = FirebaseAuth.instance;
   final firestore = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
 
   final formKey = GlobalKey<FormState>();
+  bool uploading = false;
+  String imageUrl ='';
 
   @override
   void initState() {
     super.initState();
     titleController.text = widget.note.title;
     contentController.text = widget.note.content;
+    imageUrl=widget.note.image;
   }
 
   @override
@@ -42,6 +47,24 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
           key: formKey,
           child: Column(
             children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(50),
+                    onTap: () => pickImage(),
+                    child: Container(
+                      height: 150,
+                      width: double.infinity,
+                      child: Image.network(imageUrl),
+                    ),
+                  ),
+                  Visibility(
+                    visible: uploading,
+                    child: const CircularProgressIndicator(),
+                  ),
+                ],
+              ),
               const SizedBox(height: 10),
               TextFormField(
                 controller: titleController,
@@ -99,14 +122,66 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
 
     String title = titleController.text;
     String content = contentController.text;
+    String image = imageUrl;
 
     widget.note.title = title;
     widget.note.content = content;
+    widget.note.image = image;
 
     firestore.collection("name")
         .doc(widget.note.id)
         .update(widget.note.toMap());
 
     Navigator.pop(context, widget.note);
+  }
+
+  void pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    // Pick an image.
+    final XFile? file = await picker.pickImage(source: ImageSource.gallery);
+
+    File image = File(file!.path);
+
+    uploadImage(image);
+  }
+
+  void uploadImage(File image) {
+    setState(() {
+      uploading = true;
+    });
+    final userId = auth.currentUser!.uid;
+
+    storage.ref("profileImages/$userId").putFile(image).then((value) {
+      print('uploadImage => SUCCESS');
+      getImageUrl();
+    }).catchError((error) {
+      setState(() {
+        uploading = false;
+      });
+      print('uploadImage => $error');
+    });
+  }
+
+  void getImageUrl() {
+    final userId = auth.currentUser!.uid;
+
+    storage.ref("profileImages/$userId").getDownloadURL().then((value) {
+      print('getImageUrl => $value');
+      setState(() {
+        imageUrl = value;
+        uploading = false;
+      });
+      saveImageUrl(imageUrl);
+    }).catchError((error) {
+      print('getImageUrl => $error');
+    });
+  }
+
+  void saveImageUrl(String imageUrl) {
+    final userId = auth.currentUser!.uid;
+
+    firestore.collection("name").doc(userId).update({
+      'imageUrl': imageUrl,
+    });
   }
 }
